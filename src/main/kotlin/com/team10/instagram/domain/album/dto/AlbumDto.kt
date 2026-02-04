2672f5459e77856e510434e222f70170d79f922d
SeongHeon Kim <sheon2001@gmail.com>
2026-02-04 17:12:14 +0900

    [fix] limit album title to 50 characters

--- src/main/kotlin/com/team10/instagram/domain/album/dto/AlbumDto.kt
+++ src/main/kotlin/com/team10/instagram/domain/album/dto/AlbumDto.kt
@@ -1,6 +1,11 @@
 package com.team10.instagram.domain.album.dto
 
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Size
+
 data class AlbumCreateRequest(
+    @field:NotBlank(message = "앨범 제목은 필수입니다.")
+    @field:Size(max = 50, message = "앨범 제목은 50자를 초과할 수 없습니다.")
     val title: String,
 )
 
